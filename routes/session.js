f62f0c9280758a10870f65c50b0ec5a0a545ac88
BarenyaKumar <barenyakumar@gmail.com>
2022-07-01 22:20:59 +0530

    small changes

--- routes/session.js
+++ routes/session.js
@@ -3,7 +3,38 @@ const router = express.Router()
 const Booking = require("../models/booking")
 const Session = require("../models/oneSession")
 
+var jwt = require("jsonwebtoken")
+var uuid4 = require("uuid4")
+var app_access_key = "62a5e852b873787aa2709787"
+var app_secret =
+  "ADM7Q7WkDeJSAIvu-gijRM27381cfXsjZH_eMZjxG0XM1DlAr7kQ6X4Vyc7pgj0x2fEiNOAYBDXwe4vtmBCkFQn9diy8TArRev_Q5pQUdwqGp40tH-ORzlC7f6mNmcIWCKr1aWioVrXWJsi-teGGFfiQa6EBP9upirTEfCVpz0M="
 
+
+router.get("/token", async (req, res) => {
+  try {
+    jwt.sign(
+      {
+        access_key: app_access_key,
+        type: "management",
+        version: 2,
+        iat: Math.floor(Date.now() / 1000),
+        nbf: Math.floor(Date.now() / 1000),
+      },
+      app_secret,
+      {
+        algorithm: "HS256",
+        expiresIn: "24h",
+        jwtid: uuid4(),
+      },
+      function (err, token) {
+        console.log(token)
+        res.status(200).json(token)
+      }
+    )
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 //create session
 // router.post('/', async(req, res) => {
 //   try {
